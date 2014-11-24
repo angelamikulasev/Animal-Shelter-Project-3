9bbb76dfbcadb1951642b2c73f989b40f1954f15
angelamikulasev <angela.mikulasev@hotmail.com>
2014-11-24 11:02:33 +1100

    working on links

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,11 @@ annie = User.create!(
 )
 
 addy = User.create!(
-  fistname: addy,
-  lastname: mikulasev,
-  email: addy@addy.com,
-  password: addy,
-  password_confirmation: addy
+  firstname: 'addy',
+  lastname: 'mikulasev',
+  email: 'addy@addy.com',
+  password: 'addy',
+  password_confirmation: 'addy'
 )
 
 %w[Dogs Cats Reptiles Rabbits].each do |category|
@@ -20,24 +20,26 @@ end
 
 buddy = Animal.create!(
   adoptee_id: annie.id,
-  name: buddy,
-  description: Brown Labrador,
-  age: 2,
-  species: Labrador,
-  gender: Male,
-  image:
-  category_id: Category.where(name: 'Dogs').first.id 
+  name: 'Buddy',
+  about_me: 'I am a dog',
+  ideal_home: 'Big backyard',
+  species: 'Dog',
+  gender: 'male',
+  child_friendly: 'true',
+  image: 'http://a1.dspnimg.com/data/g/426128089239_2H1pNAoy_g.jpg',
+  # category_id: Category.where(name: 'Dogs').first.id 
 )
 
 buddy.update_attributes!(adopter_id: addy.id)
 
 lily = Animal.create!(
-  adoptee_id: ,
-  name: lily,
-  description: Ragdoll kitten,
-  age: 1,
-  species: Ragdoll,
-  gender: Female,
-  image: 
-  category_id: Category.where(name: 'Cats').first.id
+  adoptee_id: annie.id,
+  name: 'Lily',
+  about_me: 'I am a cat',
+  ideal_home: 'Cosy home',
+  species: 'Cat',
+  gender: 'female',
+  child_friendly: 'true',
+  image: 'http://a1.dspnimg.com/data/l/3225228245989_68tEtJNp_l.jpg',
+  # category_id: Category.where(name: 'Cats').first.id
 )
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  it { is_expected.to has_many(:adoptions) }
-  it { is_expected.to has_many(:adoptees, class_name: "Animal", foreign_key: "adoptee_id") }
-  it { is_expected.to has_many(:adopters, class_name: "Animal", foreign_key: "adopter_id") }
+  it { should have_many(:adoptions) }
+  it { is_expected.to have_many(:adoptees, class_name: "Animal", foreign_key: "adoptee_id") }
+  it { is_expected.to have_many(:adopters, class_name: "Animal", foreign_key: "adopter_id") }
 end
 
   # has_many :adoptions
