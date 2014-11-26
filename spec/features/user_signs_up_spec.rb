ef6792b2bd1051a1f5f91be0dd584c258c134edc
angelamikulasev <angela.mikulasev@hotmail.com>
2014-11-27 01:04:34 +1100

    user_signs_up_spec

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -1,20 +1,34 @@
-require 'rails_helper'
+require 'capybara/rspec'
+require 'spec_helper'
+require 'pry'
 
 feature 'User signs up' do 
   given(:firstname) { 'Annie' }
-  given(:password) { 'chicken' }
 
   scenario 'with valid details' do
-
     visit new_user_path
     fill_in 'user[firstname]', with: firstname
     fill_in 'user[lastname]', with: 'mik'
     fill_in 'user[email]', with: 'annie@annie.com'
-    fill_in 'user[password]', with: password
-    fill_in 'user[password_confirmation]', with: password
+    fill_in 'user[password]', with: 'chicken'
     click_button 'Create User'
+
 
     expect(page).to have_content('Sign out')
     expect(page).to have_content("Welcome to Animal Rescue #{ firstname }")
   end
+
+  # scenario 'with invalid sign up' do
+  #   sign_up_with '', '', '', ''
+  #   expect(page).to have_content('Adopt', 'FAQ', 'About', 'Search for species', 'Sign up', 'Log in' )
+  # end
+
+  # def sign_up_with(firstname, lastname, email, password_digest)
+  #   visit users_path
+  #   fill_in 'Firstname', with: firstname
+  #   fill_in 'Lastname', with: lastname
+  #   fill_in 'Email', with: email
+  #   fill_in 'password', with: password_digest
+  #   click_button 'Create User'
+  # end
 end
